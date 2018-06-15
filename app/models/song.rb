88cc3ad6e5ddc3929c59049bacffdd044a46bf9d
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-15 15:32:18 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name=(artist_name)
-    binding.pry
     Artist.find_or_create_by(name: artist_name)
     self.artist = Artist.find_or_create_by(name: artist_name)
   end
