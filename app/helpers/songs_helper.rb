a57edb43aa5c800abb84ddad7c0ee752540fde5f
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-15 15:18:02 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def display_artist(artist)
+    if self.artist_id
+      link_to self.artist_name, artist_path(self.artist)
+    else
+      link_to "Add Artist", edit_song_path(self)
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
+  def artist_name=(artist_name)
+    self.artist.name = artist_name
   end
-
-  def artist_name=(name)
+  
+  def artist_name
+    self.artist.name
   end
 end
